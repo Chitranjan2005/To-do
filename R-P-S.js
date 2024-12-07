dee98d1b6d594bb87dcdd7aaf9afcd3a5254b37e
Chitranjan <ranjan888288@gmail.com>
2024-12-07 20:28:35 +0530

    Update R-P-S.js

--- R-P-S.js
+++ R-P-S.js
@@ -2,6 +2,11 @@ let playerscore = 0;
 let compscore = 0;
 
 const icons = document.querySelectorAll(".icon");
+const result = document.querySelector("h4");
+
+
+const playercount = document.querySelector("#player");
+const compcount = document.querySelector("#comp");
 
 const gencompchoice = () => {
     const options = ["rock", "paper", "scissors"];
@@ -10,29 +15,54 @@ const gencompchoice = () => {
 } 
 
 const draw = () => {
-    console.log("game is draw");
+    // console.log("game is draw");
 }
 
 const game = (playerchoice) => {
-    console.log("playerchoice =", playerchoice);
+    // console.log("playerchoice =", playerchoice);
     const compchoice = gencompchoice();
-    console.log("compchoice =", compchoice);
+    // console.log("compchoice =", compchoice);
 
-if(playerchoice === compchoice) {
+ if(playerchoice === compchoice) {
 draw();
+result.innerText = "GAME IS DRAW";
+result.style.backgroundColor = "#0a0908";
+
 } else{
     let playerwin = true;
-    if((playerchoice = "rock") && (compchoice = scissors)){
-        console.log("player is winner");
-    } else if((playerchoice = "scissors") && (compchoice = paper)) {
-        console.log("player is winner");  
-    } else if((playerchoice = "paper") && (compchoice = rock)) {
-        console.log("player is winner");
+    if((playerchoice === "rock") && (compchoice === "scissors")){
+         result.innerText = "YOU WIN";
+         result.style.backgroundColor = "#55a630";
+         playerscore++;
+         playercount.innerText = playerscore;
+         
+
+
+    } else if((playerchoice === "scissors") && (compchoice === "paper")) {
+        result.innerText = "YOU WIN";
+        result.style.backgroundColor = "#55a630";
+        playerscore++;
+        playercount.innerText = playerscore;
+
+
+
+    } else if((playerchoice === "paper") && (compchoice === "rock")) {
+        result.innerText = "YOU WIN";
+        result.style.backgroundColor = "#55a630";
+        playerscore++;
+        playercount.innerText = playerscore;
+
+
+
     }else {
-        console.log("computer is winner");
+        result.innerText = "YOU LOSE";
+        result.style.backgroundColor = "#dd1c1a";
+        compscore++
+        compcount.innerText = compscore;
     }
 }
 }
+
 
 icons.forEach((icon) => {
     icon.addEventListener("click", () => {
